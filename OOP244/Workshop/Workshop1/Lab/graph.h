48f8586d07ea6897a672ee413b5789b142c39032
sanghyuk2 <41242710+sanghyuk2@users.noreply.github.com>
2018-11-29 17:36:24 -0500

    Add files via upload

---
+++ OOP244/Workshop/Workshop1/Lab/graph.h
@@ -0,0 +1,17 @@
+/**********************************************************
+* Name: SangHyuk Lee
+* Student ID: 129405171
+* Seneca email: slee395@myseneca.ca
+* Date of Completion: 5/18/2018
+**********************************************************/
+#define MAX_NO_OF_SAMPLES 20
+
+// Fills the samples array with the statistic samples
+void getSamples(int samples[], int noOfSamples);
+// Finds the largest sample in the samples array, if it is larger than 70,
+// therwise it will return 70. 
+int findMax(int samples[], int noOfSamples);
+// Prints a scaled bar relevant to the maximum value in samples array
+void printBar(int val, int max);
+// Prints a graph comparing the sample values visually 
+void printGraph(int samples[], int noOfSamples);
---
+++ OOP244/Workshop/Workshop1/Lab/tools.h
@@ -0,0 +1,9 @@
+/**********************************************************
+* Name: SangHyuk Lee
+* Student ID: 129405171
+* Seneca email: slee395@myseneca.ca
+* Date of Completion: 5/18/2018
+**********************************************************/
+
+int menu();
+int getInt(int min, int max);
