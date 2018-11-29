25a2370da453bef343e24f025d16ff8ba928577d
sanghyuk2 <41242710+sanghyuk2@users.noreply.github.com>
2018-11-29 17:48:21 -0500

    Add files via upload

---
+++ OOP244/Workshop/Workshop5/Lab/Fraction.cpp
@@ -0,0 +1,99 @@
+/**********************************************************
+* Name: SangHyuk Lee
+* Student ID: 129405171
+* Seneca email: slee395@myseneca.ca
+* Date of Completion: 6/15/2018
+**********************************************************/
+// TODO: insert header files
+#include <iostream>
+#include "Fraction.h"
+// TODO: continue the namespace
+using namespace std;
+
+namespace sict {
+	// TODO: implement the default constructor
+	Fraction::Fraction() {
+		Numerator = 0;
+		Denominator = 0;
+	}
+	// TODO: implement the two-argument constructor
+	Fraction::Fraction(int numerator, int denominator) {
+		if (numerator >= 0 && denominator > 0) {
+			Numerator = numerator;
+			Denominator = denominator;
+			reduce();
+		}
+		else
+			*this = Fraction();
+	}
+	// TODO: implement the max query
+	int Fraction::max() const {
+		if (Numerator > Denominator) {
+			return Numerator;
+		}
+		else
+			return Denominator;
+	}
+	// TODO: implement the min query
+	int Fraction::min() const {
+		if (Numerator > Denominator) {
+			return Denominator;
+		}
+		else
+			return Numerator;
+		
+	}
+	// gcd returns the greatest common divisor of num and denom
+	//
+	int Fraction::gcd() const {
+		int mn = min();  // min of numerator and denominator
+		int mx = max();  // max of numerator and denominator
+		int g_c_d = 1;
+		bool found = false;
+
+		for (int x = mn; !found && x > 0; --x) { // from mn decrement until divisor found
+			if (mx % x == 0 && mn % x == 0) {
+				found = true;
+				g_c_d = x;
+			}
+		}
+		return g_c_d;
+	}
+
+	// TODO: implement the reduce modifier
+	void Fraction::reduce() {
+		int temp = gcd();
+		Numerator = Numerator / temp;
+		Denominator = Denominator / temp;
+	}
+
+	// TODO: implement the display query
+	void Fraction::display() const{
+		if (!isEmpty()) {
+			if (Denominator == 1)
+				cout << Numerator;
+			else
+				cout << Numerator << "/" << Denominator;
+		}
+		else 
+			cout << "no fraction stored";
+		
+
+	}
+
+	// TODO: implement the isEmpty query
+	bool Fraction::isEmpty() const {
+		return Numerator == 0 && Denominator == 0;
+	}
+
+	// TODO: implement the + operator
+	Fraction Fraction::operator+(const Fraction& rhs) const {
+		Fraction sum;
+		if (!isEmpty() || !rhs.isEmpty()) {
+			sum.Denominator = Denominator * rhs.Denominator;
+			sum.Numerator = (Numerator * rhs.Denominator) + (Denominator * rhs.Numerator);
+		}
+		return sum;
+		
+	}
+}
---
+++ OOP244/Workshop/Workshop5/Lab/Fraction.h
@@ -0,0 +1,36 @@
+/**********************************************************
+* Name: SangHyuk Lee
+* Student ID: 129405171
+* Seneca email: slee395@myseneca.ca
+* Date of Completion: 6/15/2018
+**********************************************************/
+// TODO: header file guard
+#ifndef SICT_FRACTION_H
+#define SICT_FRACTION_H
+
+// TODO: create namespace
+namespace sict {
+	// TODO: define the Fraction class
+	class Fraction {
+		// TODO: declare instance variables 
+		int Numerator;
+		int Denominator;
+
+		// TODO: declare private member functions
+		int max() const;
+		int min() const;
+		void reduce();
+		int gcd() const;
+	public:
+		// TODO: declare public member functions
+		Fraction();
+		Fraction(int, int);
+		bool isEmpty() const;
+		void display() const;
+		// TODO: declare the + operator overload
+		Fraction operator+(const Fraction& rhs) const;
+	};
+
+}
+#endif // !FRACTION_H
+
