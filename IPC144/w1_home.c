e5a781dbfc8a46b013983a9e709f1339ca756760
sanghyuk2 <41242710+sanghyuk2@users.noreply.github.com>
2019-02-08 18:05:56 -0500

    Add files via upload

---
+++ IPC144/w1_home.c
@@ -0,0 +1,10 @@
+#include <stdio.h>	
+
+int main(void) //lee sang hyuk 129405171
+{
+	printf("********************************\n");
+	printf("*** Welcome to C Programming ***\n");
+	printf("********************************\n");
+
+	return 0;
+}
